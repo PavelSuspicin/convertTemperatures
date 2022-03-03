2159c3532aab6d8346a52dbc82ad28ec26d8d576
Pavel <pavelsuspicin@gmail.com>
2022-03-03 14:45:11 +0300

    030322

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+function cToF(celsius) {
+    let celTof = (celsius * 9) / 5 + 32
+    console.log(`${celsius} to ${celTof} `)
+}
+cToF(60)
 
+function fToC(fahr) {
+    let farToC = ((fahr - 32) * 5) / 9
+    console.log(`${fahr} to ${farToC}`)
+}
+fToC(45)
